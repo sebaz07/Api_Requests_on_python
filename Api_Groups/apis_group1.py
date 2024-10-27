c26f63fb16f50f1bbfe65a44dc46fd4ef8b0d284
sebas000007 <sebastian-martinez-07@hotmail.com>
2024-10-27 18:12:42 -0500

    Se actualizan grupos de Apis

--- Api_Groups/apis_group1.py
+++ Api_Groups/apis_group1.py
@@ -5,9 +5,18 @@ api_list = [
     "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0",
     "https://pokeapi.co/api/v2/type/3",
     "https://dragonball-api.com/api/characters/24",
-    "https://api.example.com/users", #api falsa
-    "https://pokeapi.co/api/v2/type/?", #api error
-    "https://pokeapi.co/api/v2/type/{}", #api error
-    "https://httpbin.org/status/300" #api 300 code
-
+    "https://api.example.com/users",  # api falsa
+    "https://pokeapi.co/api/v2/type/?",  # api error
+    "https://pokeapi.co/api/v2/type/{}",  # api error
+    "https://httpbin.org/status/300",  # api 300 code
+    "https://api.spacexdata.com/v4/launches/latest",
+    "https://dog.ceo/api/breeds/image/random",
+    "https://api.coindesk.com/v1/bpi/currentprice.json",
+    "https://api.agify.io?name=michael",
+    "https://api.genderize.io?name=luc",
+    "https://api.nationalize.io?name=nathaniel",
+    "https://api.chucknorris.io/jokes/random",
+    "https://catfact.ninja/fact",
+    "https://api.adviceslip.com/advice",
+    "https://api.exchangerate-api.com/v4/latest/USD"
 ]
